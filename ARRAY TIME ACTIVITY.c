82bc93d585ce8ffddd7e38bbb8e7d8fb0945b4ba
JpYoshi25 <79734494+JpYoshi25@users.noreply.github.com>
2021-04-14 09:15:13 +0800

    Update ARRAY TIME ACTIVITY.c

--- ARRAY TIME ACTIVITY.c
+++ ARRAY TIME ACTIVITY.c
@@ -5,14 +5,9 @@
 int main()
 {
     int account[3][2]={
-    {11234,43211},
-    {56789,98765},
-    {12345,54321}};
-
-    int pass[3][2]={
-    {1123,4321},
-    {5678,9876},
-    {1234,4321}};
+    {11234,3211},
+    {56789,8765},
+    {12345,4321}};
     int idnum,pin;
 
     printf("Enter your ID number: ");
@@ -22,25 +17,25 @@ int main()
 
     if (idnum==account[0][0])
     {
-        if(pin==pass[0][0])
+        if(pin==pass[0][1])
         {
             printf("\nYou have successfully logged in\n\n ID:%d",idnum);
         }
         else
         printf("\nInvalid ID/PIN!");
     }
-    else if (idnum==account[0][1])
-    {
-        if (pin==pass[0][1])
-        {
-            printf("\nYou have successfully logged in\n\n ID:%d",idnum);
-        }
-        else
-        printf("\nInvalid ID/PIN!");
-    }
-    else if (idnum==account[1][1])
+    else if (idnum==account[1][0])
     {
         if (pin==pass[1][1])
+        {
+            printf("\nYou have successfully logged in\n\n ID:%d",idnum);
+        }
+        else
+        printf("\nInvalid ID/PIN!");
+    }
+    else if (idnum==account[2][0])
+    {
+        if (pin==pass[2][2])
         {
             printf("\nYou have successfully logged in\n\n ID:%d",idnum);
         }
